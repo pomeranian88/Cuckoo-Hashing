687006ce8ce01c706004854366860cd3fe1757a9
pomeranian88 <zevtobythompson@gmail.com>
2026-02-16 20:20:16 -0600

    second exemplary push 2/16 8:20pm

--- src/main/kotlin/CuckooHashMap.kt
+++ src/main/kotlin/CuckooHashMap.kt
@@ -6,18 +6,18 @@ class CuckooHashMap<K, V>(tableSize: Int) : CuckooSetup<K, V>(tableSize) {
     // Look up a key, and get the corresponding value. Try both tables as
     // needed. Returns null if not found.
     fun get(key: K): V? {
-        var code: Int = cuckooHashCode(0, key) % tableSize
-        if(tables[0][code]==null){
-             code = cuckooHashCode(1, key) % tableSize
-             return tables[1][code]?.value
+        var code: Int = cuckooHashCode(0, key) % tableSize // get the code of the key for table 0
+        if(tables[0][code]==null){                         // if theres nothing there...
+             code = cuckooHashCode(1, key) % tableSize     //   get the code of the key for table 1
+             return tables[1][code]?.value                 //   and return its value at table 1 even if its null
         }
-        return tables[0][code]?.value
-    }
+        return tables[0][code]?.value                      // otherwise return the value at table 0, and we know its not null
+    }  
 
     // Adds a key and a value to one of the hash tables.
-    fun set(key: K, value: V) {
-        val code: Int = cuckooHashCode(0, key) % tableSize
-        tables[0][code] = Entry(key, value)
+    fun set(key: K, value: V) { // for now, only inserts to table 0
+        val code: Int = cuckooHashCode(0, key) % tableSize  // get the corresponding index for table 0
+        tables[0][code] = Entry(key, value)                 // and plug the key in at that index
     }
 
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,11 @@
+/*
+ * Author: Zev Thompson
+ * 
+ * Collab Statement: Wrote the code by myself but talked through the logic of the get and set functions with Ian.
+ * 
+ * Reflection:  Was not so hard, mainly just in understanding how arrays work, and drawing out how hash tables work here.
+ *              But because no collision detection was needed it was fairly simple. Took around 1 hour.
+ */
 
 fun main() {
     val map = CuckooHashMap<Int, String>(20)
